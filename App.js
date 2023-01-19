df5fccb76ec55043142466e71ca7ae567e012958
Niklas Hietala <niklas.hietala1997@gmail.com>
2023-01-19 17:06:57 +0200

    Tehtävä 1

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+const [result, setResult] = useState('');
+const [input, setInput] = useState('');
+const [input1, setInput1] = useState('');
+
+const add = () => {
+setResult(parseInt(input) + parseInt(input1));
+}
+
+const subtract = () => {
+setResult(parseInt(input) - parseInt(input1))
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Result: {result}</Text>
+      <View>
+<TextInput keyboardType='number-pad' style={styles.input} onChangeText={setInput}></TextInput>
+<TextInput keyboardType='number-pad' style={styles.input} onChangeText={setInput1}></TextInput>
+      </View>
+      <View style={styles.operators}>
+      <View style={styles.buttons}>
+        <Button onPress={add} title="+"/>
+        </View>
+        <View style={styles.buttons}>
+        <Button onPress={subtract} title="-"/>
+        </View>
+        </View>
+<StatusBar style='auto'/>
+      
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container: { 
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  input: {
+    borderWidth: 1,
+    height: 30,
+    width: 120,
+borderColor: 'black',
+padding: 5
+},
+ buttons: {
+  width: '15%',
+marigin: 10,
+borderColor: 'black',
+borderWidth: 1,
+marginTop: 10
+ },
+ operators: {
+flexDirection: 'row',
+justifyContent: 'space-evenly'
+ },
+ text: {
+  marginBottom: 10
+ }
 });
